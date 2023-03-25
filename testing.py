d2b309b640550a94ba19dab4081fe785ebced078
Liam Richardson <captainoblivion@gmail.com>
2023-03-25 12:13:17 -0600

    bind

--- testing.py
+++ testing.py
@@ -7,6 +7,7 @@ bufferSize = 1024
 class Client:
     def __init__(self):
       self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+      self.socket.bind(trackerIP, trackerPort)
       print("initialized")
     def main(self):
        while True:
