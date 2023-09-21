8337748e9b7e6e3a1b2a5d8c6bfc1aec6d9f8551
Liam Richardson <captainoblivion@gmail.com>
2023-09-20 20:13:50 -0600

    trying another thing

--- startup.sh
+++ startup.sh
@@ -5,9 +5,9 @@ cd $(dirname "$0")
 pwd
 
 echo "starting tracker on screen tracker"
-screen -d -m -S tracker sh tracker.sh
+screen -d -m -S tracker bash -c "tracker.sh; exec sh"
 
 echo "starting servo controller on screen pituber"
-screen -d -m -S pituber sh start.sh
+screen -d -m -S pituber bash -c "start.sh; exec sh"
 
 screen -ls
