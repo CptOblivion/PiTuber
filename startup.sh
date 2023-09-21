2bbb5f1eef322abf47056cecd13b296268552c1c
Liam Richardson <captainoblivion@gmail.com>
2023-09-20 18:34:07 -0600

    scripting

---
+++ startup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+screen -d -S tracker -m tracker.sh
+
+screen -d -S pituber -m start.sh
--- tracker.sh
+++ tracker.sh
@@ -1,3 +1,4 @@
-cd ../OpenSeeFace
+#!/bin/bash
 
+cd ../OpenSeeFace
 python3 facetracker.py --gaze-tracking 0 --model -3
