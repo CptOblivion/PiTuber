dd44a4830f5412cc2ee78fa2fc2faf81efa03631
Liam Richardson <captainoblivion@gmail.com>
2023-09-20 19:09:23 -0600

    prints

--- startup.sh
+++ startup.sh
@@ -2,6 +2,10 @@
 
 cd $(dirname "$0")
 
+pwd
+
+echo "starting tracker on screen tracker"
 screen -d -S tracker -m /tracker.sh
 
+echo "starting servo controller on screen pituber"
 screen -d -S pituber -m ./start.sh
